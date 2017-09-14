aa79043f6cb5715fe30e5ecb39137e30bdc724b0
MPOstermann <mars15b@hotmail.com>
2017-09-13 23:43:51 -0700

    Update score column in segments table to only accept numeric input

---
+++ TouhouSplits/TouhouSplits.UI/Control/DataGrid/DataGridLongColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Windows.Controls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TouhouSplits.UI.Control.DataGrid
+{
+    public class DataGridLongColumn : DataGridTextColumn
+    {
+        protected override object PrepareCellForEdit(FrameworkElement editingElement, RoutedEventArgs editingEventArgs)
+        {
+            TextBox edit = editingElement as TextBox;
+            edit.PreviewTextInput += OnPreviewTextInput;
+
+            return base.PrepareCellForEdit(editingElement, editingEventArgs);
+        }
+
+        void OnPreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            long value;
+            if (!long.TryParse(e.Text, out value)) {
+                e.Handled = true;
+            }
+            else if (value < 0) {
+                e.Handled = true;
+            }
+        }
+    }
+}
