0c58da2c49530d197317b76f1112c4e6e714478b
MPOstermann <mars15b@hotmail.com>
2017-09-15 21:52:31 -0700

    Initial implementation for GameConfig and ConfigManager

---
+++ TouhouSplits/TouhouSplits.Service/Config/GameConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace TouhouSplits.Service.Config
+{
+    public class GameConfig : IGameConfig
+    {
+        public string GameName { get; private set; } 
+        public byte ScoreMemoryLocation { get; private set; } 
+        public Type HookStrategy { get; private set; }
+
+        public GameConfig(XElement configElement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TouhouSplits/TouhouSplits.Service/Managers/Config/ConfigManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using TouhouSplits.Manager.Config;
+using TouhouSplits.Service.Config;
+
+namespace TouhouSplits.Service.Managers.Config
+{
+    public class ConfigManager : IConfigManager
+    {
+        public IList<IGameConfig> AvailableGames { get; private set; }
+
+        public ConfigManager()
+        {
+            /* Todo: load config XDocument and list of AvailableGames */
+            throw new NotImplementedException();
+        }
+
+    }
+}
