85cc98edee04d9583aa4141d67d13ed59fec1641
MPOstermann <mars15b@hotmail.com>
2017-09-16 00:09:11 -0700

    Create property for list of processes for a game

--- TouhouSplits/TouhouSplits.Service/Config/GameConfig.cs
+++ TouhouSplits/TouhouSplits.Service/Config/GameConfig.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml.Linq;
 
 namespace TouhouSplits.Service.Config
 {
     public class GameConfig : IGameConfig
     {
-        public string GameName { get; private set; } 
+        public string GameName { get; private set; }
         public byte ScoreMemoryLocation { get; private set; } 
         public XElement HookConfig { get; private set; }
+
+        private List<string> _processes;
+        public IList<string> Processes { get { return _processes.AsReadOnly(); } }
 
         public GameConfig(XElement configElement)
         {
--- TouhouSplits/TouhouSplits.Service/Config/IGameConfig.cs
+++ TouhouSplits/TouhouSplits.Service/Config/IGameConfig.cs
@@ -1,10 +1,12 @@
-﻿using System.Xml.Linq;
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace TouhouSplits.Service.Config
 {
     public interface IGameConfig
     {
         string GameName { get; }
+        IList<string> Processes { get; }
         byte ScoreMemoryLocation { get; }
         XElement HookConfig { get; }
     }
