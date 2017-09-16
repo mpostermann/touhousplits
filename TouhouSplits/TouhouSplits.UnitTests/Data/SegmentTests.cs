e00183921f8e715a562c87c675eb54977320baba
MPOstermann <mars15b@hotmail.com>
2017-09-16 09:23:28 -0700

    Add tests for Segment

---
+++ TouhouSplits/TouhouSplits.UnitTests/Data/SegmentTests.cs
@@ -0,0 +1,42 @@
+﻿using TouhouSplits.Service.Data;
+using Xunit;
+
+namespace TouhouSplits.UnitTests.Data
+{
+    public class SegmentTests
+    {
+        [Fact]
+        public void SegmentIsInitializedWithDefaultScore()
+        {
+            var segment = new Segment();
+            Assert.Equal(0, segment.Score);
+        }
+
+        [Fact]
+        public void SegmentIsInitializedWithDefaultName()
+        {
+            var segment = new Segment();
+            Assert.Equal(null, segment.SegmentName);
+        }
+
+        [Fact]
+        public void ScoreIsSetWithCorrectValue()
+        {
+            var segment = new Segment();
+            long expectedValue = 15676;
+
+            segment.Score = expectedValue;
+            Assert.Equal(expectedValue, segment.Score);
+        }
+
+        [Fact]
+        public void NameIsSetWithCorrectValue()
+        {
+            var segment = new Segment();
+            string expectedName = "I am a segment. ";
+
+            segment.SegmentName = expectedName;
+            Assert.Equal(expectedName, segment.SegmentName);
+        }
+    }
+}
