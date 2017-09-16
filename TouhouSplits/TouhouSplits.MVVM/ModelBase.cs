312c872c44c395c7774b7375d5f721b34e6da30f
MPOstermann <mars15b@hotmail.com>
2017-09-15 21:45:09 -0700

    Create project for MVVM base classes

---
+++ TouhouSplits/TouhouSplits.MVVM/ModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TouhouSplits.MVVM
+{
+    public class ModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
