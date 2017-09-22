dc2674bd212129f496276eaa59170a8f68556885
MPOstermann <mars15b@hotmail.com>
2017-09-21 22:30:29 -0700

    Create utility class for testing NotifyPropertyChanged events

---
+++ TouhouSplits/TouhouSplits.UnitTests.Utils/NotifyPropertyChangedCatcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace TouhouSplits.UnitTests.Utils
+{
+    public class NotifyPropertyChangedCatcher
+    {
+        public List<string> CaughtProperties { get; private set; }
+
+        public PropertyChangedEventHandler CatchPropertyChangedEvents {
+            get {
+                return delegate (object sender, PropertyChangedEventArgs e)
+                {
+                    CaughtProperties.Add(e.PropertyName);
+                };
+            }
+        }
+
+        public NotifyPropertyChangedCatcher()
+        {
+            CaughtProperties = new List<string>();
+        }
+    }
+}
