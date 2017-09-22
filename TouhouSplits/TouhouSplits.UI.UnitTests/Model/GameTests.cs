f93184f9c258d46c7961dbde0ad4c70b416400c6
MPOstermann <mars15b@hotmail.com>
2017-09-21 21:58:01 -0700

    Define tests for Game model

---
+++ TouhouSplits/TouhouSplits.UI.UnitTests/Model/GameTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TouhouSplits.UI.UnitTests.Model
+{
+    public class GameTests
+    {
+        [Fact]
+        public void Set_GameName_Fires_NotifyPropertyChanged_Event_For_GameManager()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Set_GameName_Fires_NotifyPropertyChanged_Event_For_RecentSplits()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Set_GameName_Loads_Matching_GameManager()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Get_CurrentScore_Returns_Negative_One_If_Game_Is_Not_Polling()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void IsPolling_Is_False_After_Construction()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void StartScorePoller_Sets_IsPoller_To_True()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void StartScorePoller_Sets_IsPoller_To_False()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
