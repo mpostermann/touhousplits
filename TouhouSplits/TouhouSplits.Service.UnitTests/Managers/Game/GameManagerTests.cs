7c95cc0647f17c9e2f40c5b4622c35207b4856f9
MPOstermann <mars15b@hotmail.com>
2017-09-24 22:51:14 -0700

    Update GameManagerTests to work with new RecentSplits functionality.

--- TouhouSplits/TouhouSplits.Service.UnitTests/Managers/Game/GameManagerTests.cs
+++ TouhouSplits/TouhouSplits.Service.UnitTests/Managers/Game/GameManagerTests.cs
@@ -1,5 +1,7 @@
 ﻿using NSubstitute;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using TouhouSplits.Service.Config;
 using TouhouSplits.Service.Data;
 using TouhouSplits.Service.Hook;
@@ -11,18 +13,40 @@ namespace TouhouSplits.Service.UnitTests.Managers.Game
 {
     public class GameManagerTests
     {
+        private static IGameConfig CreateConfig(string gameName)
+        {
+            var config = Substitute.For<IGameConfig>();
+            config.RecentSplitsList.Returns(new FileInfo("somefile.ext"));
+            config.GameName.Returns(gameName);
+            return config;
+        }
+
+        private IFileSerializer<List<string>> CreateRecentSplitsSerializer(string filename, params string[] paths)
+        {
+            var serializer = Substitute.For<IFileSerializer<List<string>>>();
+            serializer.Deserialize(filename).Returns(new List<string>(paths));
+            return serializer;
+        }
+
         [Fact]
         public void GameName_Returns_GameName_From_Config()
         {
-            var config = Substitute.For<IGameConfig>();
-            config.GameName.Returns("Some game name");
-
+            var config = CreateConfig("Some game name");
+            var recentSplitsSerializer = CreateRecentSplitsSerializer(config.RecentSplitsList.FullName);
             var manager = new GameManager(
                 config,
                 Substitute.For<IHookStrategyFactory>(),
-                Substitute.For<IFileSerializer<List<string>>>()
+                recentSplitsSerializer,
+                Substitute.For<IFileSerializer<ISplits>>()
             );
+
             Assert.Equal("Some game name", manager.GameName);
+        }
+
+        [Fact]
+        public void RecentSplits_Returns_Splits_Loaded_From_Serializer()
+        {
+            throw new NotImplementedException();
         }
     }
 }
