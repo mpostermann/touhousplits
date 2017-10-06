45e68347edd43c91b6aaa5b99e9348e08c479582
MPOstermann <mars15b@hotmail.com>
2017-10-06 00:04:20 -0700

    Implemented converted for displaying empty scores in Main Window

---
+++ TouhouSplits/TouhouSplits.UI/Converter/ScoreConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TouhouSplits.UI.Converter
+{
+    public class ScoreConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try {
+                long score = long.Parse(value.ToString());
+                if (score < 0) {
+                    return string.Empty;
+                }
+                return value;
+            }
+            catch (Exception) {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try {
+                return long.Parse(value.ToString());
+            }
+            catch (Exception) {
+                return (long) -1;
+            }
+        }
+    }
+}
