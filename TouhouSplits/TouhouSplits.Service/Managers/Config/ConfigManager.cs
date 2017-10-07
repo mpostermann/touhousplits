e254394739b4a950a81b297ff4a31ae246925900
MPOstermann <mars15b@hotmail.com>
2017-10-07 14:11:03 -0700

    Update ConfigManager to throw an exception if multiple configurations with the same Id are loaded

--- TouhouSplits/TouhouSplits.Service/Managers/Config/ConfigManager.cs
+++ TouhouSplits/TouhouSplits.Service/Managers/Config/ConfigManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Xml.Linq;
 using TouhouSplits.Manager.Config;
 using TouhouSplits.Service.Config;
@@ -23,6 +24,10 @@ namespace TouhouSplits.Service.Managers.Config
 
                 var gamesList = new List<IGameConfig>();
                 foreach (XElement gameXml in gamesXml.Root.Element("Games").Elements("Game")) {
+                    var config = new GameConfig(gameXml);
+                    if (gamesList.Any(n => n.Id == config.Id)) {
+                        throw new ConfigurationErrorsException(string.Format("Game with duplicate Id \"{0}\" found", config.Id));
+                    }
                     gamesList.Add(new GameConfig(gameXml));
                 }
                 return gamesList;
