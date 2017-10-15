25ab8e1ee68ef788e96d43e8a975feefa4bec596
MPOstermann <mars15b@hotmail.com>
2017-10-14 21:11:18 -0700

    Wrote implementation for HotkeyConfig

--- TouhouSplits/TouhouSplits.Service/Config/HotkeyConfig.cs
+++ TouhouSplits/TouhouSplits.Service/Config/HotkeyConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
@@ -11,7 +12,50 @@ namespace TouhouSplits.Service.Config
 
         public HotkeyConfig(XElement configElement)
         {
-            throw new NotImplementedException();
+            if (configElement.Name != "Hotkeys") {
+                throw new ConfigurationErrorsException(string.Format("Unexpected config element {0}", configElement.Name));
+            }
+
+            _keyMap = new Dictionary<string, Keys>();
+            foreach (XElement hotkeyElement in configElement.Elements("Hotkey")) {
+                AddHotkey(_keyMap, hotkeyElement);
+            }
+        }
+
+        private static void AddHotkey(Dictionary<string, Keys> keyMap, XElement hotkeyElement)
+        {
+            if (hotkeyElement.Attribute("method") == null || string.IsNullOrEmpty(hotkeyElement.Attribute("method").Value)) {
+                throw new ConfigurationErrorsException("Hotkey must have a non-empty \"method\" attribute.");
+            }
+            string methodName = hotkeyElement.Attribute("method").Value;
+
+            if (hotkeyElement.Attribute("keys") == null || string.IsNullOrEmpty(hotkeyElement.Attribute("keys").Value)) {
+                throw new ConfigurationErrorsException("Hotkey must have a non-empty \"keys\" attribute.");
+            }
+            Keys keys = ParseKeys(hotkeyElement.Attribute("keys").Value);
+
+            keyMap.Add(methodName, keys);
+        }
+
+        private static Keys ParseKeys(string value)
+        {
+            string[] allKeys = value.Split('+');
+            Keys keys = ParseSingleKey(allKeys[0]);
+            for (int i = 1; i < allKeys.Length; i++) {
+                Keys nextKey = ParseSingleKey(allKeys[i]);
+                keys = keys | nextKey;
+            }
+
+            return keys;
+        }
+
+        private static Keys ParseSingleKey(string value)
+        {
+            Keys keys;
+            if (!Enum.TryParse<Keys>(value, out keys)) {
+                throw new ConfigurationErrorsException(string.Format("Unrecognized key \"{0}\"", value));
+            }
+            return keys;
         }
 
         public Keys GetHotkey(string method)
