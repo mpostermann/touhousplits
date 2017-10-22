c24de94798eeffb9e68301566f8a287b577cc6f1
MPOstermann <mars15b@hotmail.com>
2017-10-21 21:58:16 -0700

    Updated MainWindow score grid so that segments which are PG are highlighted green.

---
+++ TouhouSplits/TouhouSplits.UI/Converter/IsGreaterThanOrEqualConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TouhouSplits.UI.Converter
+{
+    public class IsGreaterThanOrEqualConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2) {
+                return false;
+            }
+
+            try {
+                long value0 = long.Parse(values[0].ToString());
+                long value1 = long.Parse(values[1].ToString());
+                return value0 >= value1;
+            }
+            catch (Exception) {
+                return false;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
