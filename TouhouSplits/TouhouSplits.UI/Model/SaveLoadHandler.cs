de2bbb822ee34f3dcf610b0ead724cf65ad40b0f
MPOstermann <mars15b@hotmail.com>
2017-10-23 23:49:14 -0700

    Created SaveLoadHandler class for managing the current file of MainViewModel

---
+++ TouhouSplits/TouhouSplits.UI/Model/SaveLoadHandler.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using TouhouSplits.MVVM;
+using TouhouSplits.Service;
+using TouhouSplits.Service.Data;
+
+namespace TouhouSplits.UI.Model
+{
+    public class SaveLoadHandler : ModelBase
+    {
+        private ISplitsFacade _splitsFacade;
+        private IFileHandler<ISplits> _currentSplitsFile;
+        private bool _hasUnsavedChanges;
+
+        public SaveLoadHandler(ISplitsFacade splitsFacade)
+        {
+            _splitsFacade = splitsFacade;
+            HasUnsavedChanges = false;
+        }
+
+        public string FileName {
+            get {
+                if (_currentSplitsFile != null && _currentSplitsFile.FileInfo != null) {
+                    return _currentSplitsFile.FileInfo.Name;
+                }
+                return string.Empty;
+            }
+        }
+
+        public bool HasUnsavedChanges {
+            get { return _hasUnsavedChanges; }
+            private set {
+                _hasUnsavedChanges = value;
+                NotifyPropertyChanged("HasUnsavedChanges");
+            }
+        }
+
+        /// <summary>
+        /// Returns the currently loaded splits file. Returns null if no file is loaded.
+        /// </summary>
+        public IFileHandler<ISplits> CurrentFile()
+        {
+            return _currentSplitsFile;
+        }
+
+        public void LoadSplitsFile(IFileHandler<ISplits> splitsFile)
+        {
+            /* This is kinda hacky, but before doing anything else we reference the file handler's
+             * Object property to force it to load the object from file so we know the file is loadable */
+            var splits = splitsFile.Object;
+
+            if (_currentSplitsFile != null) {
+                _currentSplitsFile.Close();
+            }
+            _currentSplitsFile = splitsFile;
+            HasUnsavedChanges = false;
+            NotifyPropertyChanged("FileName");
+        }
+
+        public bool SaveToCurrentFile(ISplits newSplits, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            var newSplitsFile = _splitsFacade.NewSplitsFile(newSplits);
+            newSplitsFile.FileInfo = _currentSplitsFile.FileInfo;
+
+            try {
+                _currentSplitsFile.Close();
+                _currentSplitsFile = newSplitsFile;
+                HasUnsavedChanges = true;
+                //_currentSplitsFile.Save();
+                //HasUnsavedChanges = false;
+            }
+            catch (Exception e) {
+                _currentSplitsFile = newSplitsFile;
+                HasUnsavedChanges = true;
+                errorMessage = e.Message;
+            }
+
+            return !HasUnsavedChanges;
+        }
+
+        public bool SaveCurrentSplitsAs(FileInfo newPath, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            FileInfo originalFileInfo = _currentSplitsFile.FileInfo;
+            try {
+                _currentSplitsFile.FileInfo = newPath;
+                _currentSplitsFile.Save();
+                HasUnsavedChanges = false;
+                return true;
+            }
+            catch (Exception e) {
+                _currentSplitsFile.FileInfo = originalFileInfo;
+                errorMessage = e.Message;
+                return false;
+            }
+        }
+    }
+}
