9076175499ebe30b56087d736f1e0ac141f7a213
MPOstermann <mars15b@hotmail.com>
2017-10-24 00:11:29 -0700

    Update MainWindow save warning text with a warning icon image

--- TouhouSplits/TouhouSplits.UI/Properties/Resources.Designer.cs
+++ TouhouSplits/TouhouSplits.UI/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace TouhouSplits.UI.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap warning {
+            get {
+                object obj = ResourceManager.GetObject("warning", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
