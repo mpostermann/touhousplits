3ce8d5d2e0a74db240cae1d27d9a3fde88385260
MPOstermann <mars15b@hotmail.com>
2017-10-24 00:33:47 -0700

    Moved TouhouSplitsIcon.ico to Resources folder

--- TouhouSplits/TouhouSplits.UI/Properties/Resources.Designer.cs
+++ TouhouSplits/TouhouSplits.UI/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace TouhouSplits.UI.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon TouhouSplitsIcon {
+            get {
+                object obj = ResourceManager.GetObject("TouhouSplitsIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap warning {
