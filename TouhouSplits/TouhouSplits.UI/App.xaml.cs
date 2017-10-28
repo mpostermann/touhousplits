5da9d8d6dc9dc4bf22acf60cb024722230855cdb
MPOstermann <mars15b@hotmail.com>
2017-10-28 14:27:21 -0700

    Update application to load by specifying a default file to start with.

--- TouhouSplits/TouhouSplits.UI/App.xaml.cs
+++ TouhouSplits/TouhouSplits.UI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 using TouhouSplits.UI.View;
 using TouhouSplits.UI.ViewModel;
 
@@ -12,8 +14,33 @@ namespace TouhouSplits.UI
         public void ApplicationStartup(object sender, StartupEventArgs e)
         {
             MainWindow window = new MainWindow();
-            window.DataContext = new MainViewModel();
+            window.DataContext = CreateViewModel(e.Args);
             window.Show();
+        }
+
+        private static MainViewModel CreateViewModel(string[] args)
+        {
+            FileInfo splitsFilePath = TryGetSplitsPath(args);
+            if (splitsFilePath != null) {
+                return new MainViewModel(splitsFilePath);
+            }
+            return new MainViewModel();
+        }
+
+        private static FileInfo TryGetSplitsPath(string[] args)
+        {
+            try {
+                if (args.Length > 0) {
+                    FileInfo path = new FileInfo(args[0]);
+                    if (path.Exists) {
+                        return path;
+                    }
+                }
+            }
+            catch (Exception e) {
+                // Do nothing
+            }
+            return null;
         }
     }
 }
