42cc0e4283d45abec5f98ae493c3bf7185cec4f0
Paul Ostermann <mars15b@hotmail.com>
2023-12-15 14:32:28 -0800

    spike/12 - Implement score tracking for DonpachiJ

--- TouhouSplits-MAME-Plugin/plugins/touhousplits/init.lua
+++ TouhouSplits-MAME-Plugin/plugins/touhousplits/init.lua
@@ -12,7 +12,14 @@ exports.author = { name = "MPOstermann" }
 
 local touhousplits = exports
 
-local frameCount = 0
+local cpu
+local mem
+
+local pipeFile
+local readyToWrite = true
+local currentFrame = 0
+local framesBetweenWrite = 2
+
 local supportedRoms = {	["donpachij"] = true }
 
 function touhousplits.set_folder(path)
@@ -24,45 +31,59 @@ local pipe = winapi.make_pipe_server(function(file, error)
 		emu.print_info("Error occurred while establishing pipe: " .. error)
 	else
 		emu.print_info("Client connected")
+		pipeFile = file
 
-		while true do
-			local success, errorMsg = pcall(function() return file:write(frameCount .. "\n") end)
-			if not success then
-				emu.print_info("Error writing to pipe: " .. errorMsg)
-				break
-			end
-			frameCount = frameCount + 1
+		-- while true do
+		-- 	if mem ~= nil and file ~= nil then
+		-- 		local score = mem:read_u32(tonumber("10177A", 16))
+		-- 		local success, errorMsg = pcall(function() return file:write(string.format("%08x", score) .. "\n") end)
+		-- 		if not success then
+		-- 			emu.print_info("Error writing to pipe: " .. errorMsg)
+		-- 		end
 
-			winapi.sleep(50)
-		end
-		-- local readThread = file:read_asy           nc(function(line)
-		-- 	local success, errorMsg = pcall(function() return file:write(frameCount .. "\n") end)
-		-- 	if not success then
-		-- 		emu.print_info("Error writing to pipe: " .. errorMsg)
+		-- 		winapi.sleep(40)
 		-- 	end
-		-- 	frameCount = frameCount + 1
-		-- end)
+		-- end
 	end
 end)
 
 function touhousplits.startplugin()
 
 	emu.register_start(function()
-		emu.print_info("Lua version")
-		emu.print_info(_VERSION)
-
-		frameCount = 0
-		emu.print_info(emu.romname())
+		if supportedRoms[emu.romname()] then
+			emu.print_info("Loading script for " .. emu.romname())
+			cpu = manager.machine.devices["maincpu"];
+			if not cpu then
+				emu.print_error("maincpu device not found")
+			else
+				mem = cpu.spaces["program"]
+				if not mem then
+					error("program space not found")
+				end
+			end
+		end
 	end)
 
 	emu.register_frame(function()
-		-- if supportedRoms[emu.romname()] then
-		-- 	frameCount = frameCount + 1
-		-- end
+		if readyToWrite then
+			if mem ~= nil and pipeFile ~= nil and currentFrame > framesBetweenWrite then
+				readyToWrite = false
+				local score = mem:read_u32(tonumber("10177A", 16))
+				local success, errorMsg = pcall(function() return pipeFile:write(string.format("%08x", score) .. "\n") end) -- TODO: Make this non-blocking
+				if not success then
+					emu.print_info("Error writing to pipe: " .. errorMsg)
+				end
+
+				readyToWrite = true
+				currentFrame = 0
+			end
+			currentFrame = currentFrame + 1
+		end
 	end)
 
 	emu.register_stop(function()
-		
+		mem = nil
+		cpu = nil
 	end)
 end
 
